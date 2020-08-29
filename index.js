a6123d9b25c69645a3b3709e97d1555b009a929e
ThaddG <45907136+ThaddG@users.noreply.github.com>
2020-08-28 23:13:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+const puppeteer = require('puppeteer');
+const nodemailer = require('nodemailer'); //currently not using cuz i can't get it to work with puppeteer
+const cron = require('node-cron');
+require('dotenv').config();
+
+// i think there is a puppeteer function for this called page.waitFor()
+// but this code works
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+let classIsOpen = 'NOT OPEN';
+
+const Email = {
+  sendEmail: function(mailContent) {
+  
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAILPASSWORD
+      }
+    });
+
+    let mailOptions = {
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: 'CRN 12027 Class Check',
+      text: mailContent
+    };
+
+    transporter.sendMail(mailOptions, async function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+
+  }
+}
+
+
+cron.schedule('*/5 * * * *', () => {
+  peep = async function () {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.on('console', (msg) => console[msg._type]('PAGE LOG:', msg._text));
+    await page.goto('https://registration.wayne.edu/StudentRegistrationSsb/ssb/classRegistration/classRegistration');
+    // other actions...
+    await page.screenshot({path: 'testingthepuppet.png'});
+    // LOGIN
+    await page.type('#accessid', process.env.ACCESSID);
+    await page.type('#passwd', process.env.WSUPASSWORD);
+    await page.click('#login-button');
+    await page.waitForNavigation({waitUntil: 'load'});
+  
+      // then snapshot that
+    await page.screenshot({path: 'testingthepuppet2.png'});
+  
+    // CLICK THE REGISTER BUTTON
+    await page.click('#classSearchLink');
+  
+    await page.screenshot({path: 'testingthepuppet3.png'});
+  
+    // SELECT A TERM
+    await page.click('.select2-choice');
+    await delay(3000);
+    // try {
+    //   await page.waitFor('#202009', {visible:true})
+    // } catch (error) {
+    //   console.log("The element didn't appear")
+    // }
+    await page.keyboard.press('Enter');
+    await page.click('#term-go');
+  
+    await page.screenshot({path: 'testingthepuppet5.png'});
+  
+    await page.waitForSelector('#txt_courseNumber', {visible:true})
+    await page.type('#txt_courseNumber', process.env.COURSENUMBER);
+    await page.type('#txt_keywordlike', process.env.CRN);
+    await page.click('#search-go');
+  
+    await page.waitForSelector('.status-bold', {visible:true})
+  
+    await page.screenshot({path: 'testingthepuppet6.png'});
+  
+    let mob = await page.evaluate((classIsOpen) => {
+      let meep = document.querySelector('.status-bold');
+      if (meep.innerHTML === "FULL") {
+        console.log("STILL FULL");
+        classIsOpen = "STILL FULL"
+      } else {
+        console.log("OPEN...or something went wrong");
+        classIsOpen = "IT'S OPEN...or something went wrong"
+      }
+      return classIsOpen
+    }, classIsOpen)
+    // await page.screenshot({path: 'testingthepuppet7.png'});
+  
+    await browser.close();
+    return mob;
+  };
+  
+  peep()
+    .then(res => {
+      console.log("result", res);
+      Email.sendEmail(res);
+    })
+});
